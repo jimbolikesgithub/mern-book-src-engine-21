2f401075221d17f814510031941c609e3553fe82
James Edwards <edwards.econn@gmail.com>
2022-05-30 23:07:09 -0400

    Pushing new changes to heroku

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,43 @@ const typeDefs = gql`
         username: String!
         email: String!
         password: String!
-        savedBooks: [book]
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: String!
+        authors: [String!]
+        description: String!
+        title: String!
+        image:  String
+        link: String
+    }
+
+    input BookInput {
+        title: String!
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
     }
 `;
 module.exports = typeDefs;
+// DONE
